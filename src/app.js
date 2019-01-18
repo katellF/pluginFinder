7bd14241ba6f21f6e750240d45e25aa438f352ae
katell <katell.fracassi@gmail.com>
2019-01-18 12:00:49 +0100

    component Hit created

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {Config} from './config.js';
 import './styles/styles.css';
 //import App from './components/App';
+import Hit from './components/Hit';
+
 import {
     InstantSearch,
     SearchBox,
@@ -14,27 +16,27 @@ import {
     RefinementList
 } from 'react-instantsearch/dom';
 
-const Hit = ({hit}) =>
-    <div className="hit">
-        <div className="hit-image">
-            <img width="100" height="100" src={hit.img_thumb} alt="images"/>
-        </div>
-        <div className="hit-content">
-            <div className="hit-name">
-                <Highlight attribute="name" hit={hit}/>
-            </div>
-            <div className="hit-description">
-                <Highlight attribute="short_description" hit={hit}/>
-            </div>
-            <button className="hit-favorits">
-                My favorites
-            </button>
-            <button className="hit-info">
-                More Info
-            </button>
-
-        </div>
-    </div>
+// const Hit = ({hit}) =>
+//     <div className="hit">
+//         <div className="hit-image">
+//             <img width="100" height="100" src={hit.img_thumb} alt="images"/>
+//         </div>
+//         <div className="hit-content">
+//             <div className="hit-name">
+//                 <Highlight attribute="name" hit={hit}/>
+//             </div>
+//             <div className="hit-description">
+//                 <Highlight attribute="short_description" hit={hit}/>
+//             </div>
+//             <button className="hit-favorits">
+//                 My favorites
+//             </button>
+//             <button className="hit-info">
+//                 More Info
+//             </button>
+//
+//         </div>
+//     </div>
 
 const Sidebar = (props) =>
 
---
+++ src/components/Hit.js
@@ -0,0 +1,36 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import {
+    InstantSearch,
+    SearchBox,
+    Hits,
+    Highlight,
+    Stats,
+    SortBy,
+    Pagination,
+    RefinementList
+} from 'react-instantsearch/dom';
+
+const  Hit = ({hit}) =>
+    <div className="hit">
+        <div className="hit-image">
+            <img width="100" height="100" src={hit.img_thumb} alt="images"/>
+        </div>
+        <div className="hit-content">
+            <div className="hit-name">
+                <Highlight attribute="name" hit={hit}/>
+            </div>
+            <div className="hit-description">
+                <Highlight attribute="short_description" hit={hit}/>
+            </div>
+            <button className="hit-favorits">
+                My favorites
+            </button>
+            <button className="hit-info">
+                More Info
+            </button>
+
+        </div>
+    </div>
+
+export default Hit;
