1ace69a846b04f1a82011c8cb5ccdf8243cd652e
katell <katell.fracassi@gmail.com>
2019-01-18 12:35:51 +0100

    Config.js ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
  import React from 'react';
- import {Config} from '../config.js';
+ import Config from '../config.js';
  import Content from './Content';
  import Sidebar from './SideBar';
  import {
@@ -12,9 +12,9 @@
         // console.log(this.props);
         return (
             <InstantSearch
-                apiKey= "fa83bb59b1a41155e0670b1869aa4df3"
-                appId= "A2MUMFS8E7"
-                indexName= "wp_plugins"
+                apiKey= {Config.apiKey}
+                appId= {Config.appId}
+                indexName={Config.index}>
                 >
                 <header className="header marginBottom50 marginTop50">
                     <SearchBox translations={{placeholder: 'Search for Plugins'}}/>
