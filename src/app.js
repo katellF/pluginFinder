82b61a65dd244ad27e1651bee176845cfa7d6331
katell <katell.fracassi@gmail.com>
2019-01-18 15:08:51 +0100

    reactRouter innstalled for nav and modal for button info

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './styles/styles.css';
 import App from './components/App';
 
---
+++ src/components/InfoModal.js
@@ -0,0 +1,2 @@
+// where the code for the modal Info will be
+// When we'll click on the button Info
