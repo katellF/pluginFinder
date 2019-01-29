b604be58d530cbfba9e5ecba856cf55476106271
katell <katell.fracassi@gmail.com>
2019-01-29 11:41:37 +0100

    normalizer installed

--- src/app.js
+++ src/app.js
@@ -1,87 +1,10 @@
-//import React from 'react';
-
-
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
-import './styles/styles.scss';
 import HomeSearch from './components/HomeSearch';
 import MySpace from './components/MySpace';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
 
-
-
-//     if ( currentPage && currentPage === "homeView") {
-//     ReactDOM.render(<HomeSearch />, document.getElementById('app'));
-// } else {
-//
-//         getAPIListFavorites().then ( data => {
-//
-//             const template = (
-//                 <div>
-//                     <h1>{data.map((json1, index) =>{
-//                         // return <p key={Math.random()}></p>
-//                         return <p key={index}>{json1.pluginName} --- {json1.pluginId}</p>
-//                         //return <p key={index}>{json1}</p>
-//                     })}</h1>
-//                 </div>
-//             );
-//
-//             ReactDOM.render( template, document.getElementById('app') );
-//
-//         });
-//
-//
-//     console.log ('no data');
-// }
-
-
-
-// async function getAPIListFavorites() {
-//
-//    // let test = [1,2,3];
-//     const response = await fetch('/projetsoc/pluginfinder/index.php?action=API/listFavorites');
-//     let json = await response.json();
-//     console.log(json);
-//     // console.log(template);
-//
-//     return json;
-//
-// }
-
-
-// class MySpace extends React.Component {
-//
-//     constructor() {
-//         super();
-//         this.state = {
-//              data: [],
-//          };
-//
-//      }
-//
-//
-//     componentDidMount(){
-//
-//         fetch('http://localhost:8888/projetsoc/pluginFinder/index.php?action=API/listFavorites')
-//              .then(results => results.json())
-//              .then(json => this.setState({data:json}));
-//      };
-//
-//
-//
-//     render() {
-//         return (
-//
-//             <div>
-//                 <h1 >Mes Favoris</h1>
-//
-//                 <p>{this.state.data.map((json, index) =>
-//
-//                     <p key={index}>{json.pluginName} --- {json.pluginId}</p>
-//                 )}</p>
-//             </div>
-//         );
-//     }
-// }
 
 if ( currentPage && currentPage === "homeView") {
  ReactDOM.render(<HomeSearch />, document.getElementById('app'));
