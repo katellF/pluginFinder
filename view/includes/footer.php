f96a4ae6ea97ccaf9606d0a6c2f1b83d870469ba
katell <katell.fracassi@gmail.com>
2019-01-31 11:45:58 +0100

    footer.php updated

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -6,11 +6,7 @@
             <div class="navbar-collapse row justify-content-center" id="navbarTogglerDemo03">
                 <ul class="navbar-nav flex_rowR">
                     <li class="nav-item active margin_rightR">
-                        <a class="nav-link" href="index.php?action=connection">Admin <span
-                                    class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item active margin_rightR">
-                        <a class="nav-link" href="#">Mentions Légales</a>
+                        <a class="nav-link" href="#">Legal Notice</a>
                     </li>
                     <li class="nav-item active margin_rightR">
                         <a class="nav-link" href="index.php?action=contact">Contact</a>
