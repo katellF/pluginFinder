a790cc041bd66c8b19054984d21b9f19368b7023
katell <katell.fracassi@gmail.com>
2019-02-06 13:05:28 +0100

    remove/favorite button step 0ne

--- src/components/Hit.js
+++ src/components/Hit.js
@@ -7,101 +7,124 @@ class Hit extends React.Component {
 
     constructor(props) {
         super(props);
-        //this.addFavorite = this.addFavorite.bind(this);
-       // this.removeFavorite = this.removeFavorite.bind(this);
-        // this.state = {
-        //     data: [],
-        // };
+        this.addFavorite = this.addFavorite.bind(this);
+        this.removeFavorite = this.removeFavorite.bind(this);
+        //this.apiAddFavorite = this.apiAddFavorite.bind(this);
+        this.state = {
+             isFavorite: false,
+         };
     }
 
 
-    addFavorite(e) {
-
-        console.log("favortie from hit component");
-        let clickedPluginId = jQuery(e.currentTarget).data('id');
-        let clickedPluginName = jQuery(e.currentTarget).data('name');
-        console.log(clickedPluginId);
-        console.log(clickedPluginName);
-        //console.log(this.removeFavorite);
-
-        jQuery.ajax({
-        method: "GET",
-        url: Config.base_url + "/pluginfinder/index.php",
-        dataType: "json",
-        data: 'action=API/addFavorite/'+clickedPluginId+'/'+clickedPluginName+'/',
-         success:
-             function() {
-             $('#'+clickedPluginId).replaceWith( "<p class=\"hit_center\"><button class=\"hit-favorites hit__add hit--addLink\" onclick=\"this.removeFavorite\" data-id="+clickedPluginId+" data-name="+clickedPluginName+" id="+clickedPluginId+">Remove</button></p>");
-             listFavoritesIds.push(clickedPluginId);
-         console.log("Ca marche addFavorite");
-             console.log("this",this);
-             console.log("SUCCESSssssss");
-        }
-        ,
-        error: function(e) {
-            console.log(e);
-             console.log('ERROR');
-         }
-    })
-    }
-
-    // addFavorite(){
+    // addFavorite(e) {
     //
-    //     this.componentDidMount();
+    //     console.log("favortie from hit component");
+    //     let clickedPluginId = jQuery(e.currentTarget).data('id');
+    //     let clickedPluginName = jQuery(e.currentTarget).data('name');
+    //     console.log(clickedPluginId);
+    //     console.log(clickedPluginName);
+    //     //console.log(this.removeFavorite);
+    //
+    //     jQuery.ajax({
+    //     method: "GET",
+    //     url: Config.base_url + "/pluginfinder/index.php",
+    //     dataType: "json",
+    //     data: 'action=API/addFavorite/'+clickedPluginId+'/'+clickedPluginName+'/',
+    //      success:
+    //          function() {
+    //          $('#'+clickedPluginId).replaceWith( "<p class=\"hit_center\"><isFavorite class=\"hit-favorites hit__add hit--addLink\" onclick=\"this.removeFavorite\" data-id="+clickedPluginId+" data-name="+clickedPluginName+" id="+clickedPluginId+">Remove</isFavorite></p>");
+    //          listFavoritesIds.push(clickedPluginId);
+    //      console.log("Ca marche addFavorite");
+    //          console.log("this",this);
+    //          console.log("SUCCESSssssss");
+    //     }
+    //     ,
+    //     error: function(e) {
+    //         console.log(e);
+    //          console.log('ERROR');
+    //      }
+    // })
     // }
+
+    addFavorite(){
+
+        this.setState((prevState) => {
+            return {
+                isFavorite: !prevState.isFavorite
+
+            };
+
+         });
+
+        console.log(this.state.isFavorite);
+    }
+
+    removeFavorite(){
+
+        this.setState((prevState) => {
+            return {
+                isFavorite: !prevState.isFavorite
+            }
+
+        });
+
+        console.log(this.state.isFavorite);
+    }
     //
-    // componentDidMount(){
+
+
+    // apiAddFavorite(){
     //
-    //    // let clickedPluginId = jQuery(e.currentTarget).data('id');
-    //    // let clickedPluginName = jQuery(e.currentTarget).data('name');
-    //
-    //     fetch(Config.base_url +'/pluginfinder/index.php?action=API/addFavorite'+this.props.id+'/'+this.props.name)
+    //     fetch(Config.base_url +'/pluginfinder/index.php?action=API/addFavorite'+'/'+this.props.id+'/'+this.props.name)
     //         .then(results => results.json())
-    //         .then(json => this.setState({data:json}));
+    //         .catch((error) => console.error(error));
+    //
     // };
 
-    removeFavorite (e) {
-
-        console.log("REMOVE from hit component");
-
-
-        let clickedPluginId = jQuery(e.currentTarget).data('id');
-        let clickedPluginName = jQuery(e.currentTarget).data('name');
-
-
-        console.log(clickedPluginId);
-
-
-        jQuery.ajax({
-            method: "GET",
-            url: Config.base_url + "/pluginfinder/index.php",
-            dataType: "json",
-            data: 'action=API/deleteFavorite/'+clickedPluginId+'/'+clickedPluginName+'/',
-            success:
-
-                function() {
-                    jQuery('#' + clickedPluginId).replaceWith("<p class=\"hit_center\"><button onclick=\"this.addFavorite\" class=\"hit-favorites hit__add hit--addLink\" data-id=" + clickedPluginId + " data-name=" + clickedPluginName + " id=" + clickedPluginId + ">Remove</button></p>");
-                    // listFavoritesIds.reverse(clickedPluginId);
-                    console.log("Ca marche removeFavorite");
-
-
-                    console.log("SUCCESS");
-                },
-
-            error: function(e) {
-                console.log(e);
-                console.log('ERROR');
-            }
-        })
-    }
+    // removeFavorite (e) {
+    //
+    //     console.log("REMOVE from hit component");
+    //
+    //
+    //     let clickedPluginId = jQuery(e.currentTarget).data('id');
+    //     let clickedPluginName = jQuery(e.currentTarget).data('name');
+    //
+    //
+    //     console.log(clickedPluginId);
+    //
+    //
+    //     jQuery.ajax({
+    //         method: "GET",
+    //         url: Config.base_url + "/pluginfinder/index.php",
+    //         dataType: "json",
+    //         data: 'action=API/deleteFavorite/'+clickedPluginId+'/'+clickedPluginName+'/',
+    //         success:
+    //
+    //             function() {
+    //                 jQuery('#' + clickedPluginId).replaceWith("<p class=\"hit_center\"><isFavorite onclick=\"this.addFavorite\" class=\"hit-favorites hit__add hit--addLink\" data-id=" + clickedPluginId + " data-name=" + clickedPluginName + " id=" + clickedPluginId + ">Remove</isFavorite></p>");
+    //                 // listFavoritesIds.reverse(clickedPluginId);
+    //                 console.log("Ca marche removeFavorite");
+    //
+    //
+    //                 console.log("SUCCESS");
+    //             },
+    //
+    //         error: function(e) {
+    //             console.log(e);
+    //             console.log('ERROR');
+    //         }
+    //     })
+    // }
 
 
 
     render() {
+
+        console.log(this.state.isFavorite);
     const {hit} =this.props;
 
 //console.log("this",this);
-    // We check if the pluginId is present in the listFavoritesIds and we show/hide the Add button as needed
+   // We check if the pluginId is present in the listFavoritesIds and we show/hide the Add isFavorite as needed
     let isInFavorites = listFavoritesIds.indexOf(hit.id);
     let isFavorite = false;
 
@@ -125,8 +148,12 @@ class Hit extends React.Component {
                     </div>
 
                     { isUserConnected
-                        ? ( isFavorite ? (<p className={"hit_center"}><button onClick={this.removeFavorite} className="hit-favorites hit__add hit--addLink" data-id={hit.id} data-name={hit.name} id={hit.id}>Remove</button></p>)
-                                : (<p className={"hit_center"}><button onClick={this.addFavorite} className="hit-favorites hit__add hit--addLink" data-id={hit.id} data-name={hit.name} id={hit.id}>Add to Favorites</button></p>)
+                        ? ( this.state.isFavorite ? (<p className={"hit_center"}><button onClick={this.removeFavorite} className="hit-favorites hit__add hit--addLink" data-id={hit.id} data-name={hit.name} id={hit.id}>Remove</button></p>)
+                                : (<p className={"hit_center"}>
+                                    <button onClick={this.addFavorite} className="hit-favorites hit__add hit--addLink" data-id={hit.id} data-name={hit.name} id={hit.id}>
+                                        Add to favorites
+                                </button>
+                                </p>)
                         ) : (<p className="hit_center hit_buttonBlue"><a href="index.php?action=connect/redirect" className={"hit__add"}>Add to Favorites</a></p>)
                     }
 
@@ -138,6 +165,52 @@ class Hit extends React.Component {
 
         );
     }
+
+
+//     render() {
+//         console.log(this.state.isFavorite);
+//         const {hit} =this.props;
+//
+//
+//         // We check if the pluginId is present in the listFavoritesIds and we show/hide the Add isFavorite as needed
+//         let isInFavorites = listFavoritesIds.indexOf(hit.id);
+//         let isFavorite = false;
+//
+//         if ( isInFavorites > -1) {
+//             isFavorite = true;
+//         }
+//
+//
+//         return (
+//
+//             <div className="hit">
+//                 <div className="hit-image">
+//                     <img width="100" height="100" src={hit.img_thumb} alt="images"/>
+//                 </div>
+//                 <div className="hit-content hitPerso">
+//                     <div className="hit-name hit__namePerso">
+//                         <Highlight attribute="name" hit={hit}/>
+//                     </div>
+//                     <div className="hit-description">
+//                         <Highlight attribute="short_description" hit={hit}/>
+//                     </div>
+//
+//                     { isUserConnected
+//                         ? (<p className={"hit_center"}><isFavorite onClick={this.addFavorite} className="hit-favorites hit__add hit--addLink" data-id={hit.id} data-name={hit.name} id={hit.id}>
+//                     {this.state.isFavorite ? 'remove' : 'add to favorites'}
+//                             </isFavorite></p>
+//                         ) : (<p className="hit_center hit_buttonBlue"><a href="index.php?action=connect/redirect" className={"hit__add"}>Add to Favorites</a></p>)
+//                     }
+//
+//                     <p className="hit_center hit_buttonGrey"><a href={hit.plugin_page_at_source} target="_blank" className="hit__moreInfo hit--moreInfoLink">More Info</a></p>
+//
+//
+//                 </div>
+//             </div>
+//
+//
+//         );
+//     }
 }
 
 export default Hit;
