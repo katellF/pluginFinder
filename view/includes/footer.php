8e9695fb2415e073a28ce48a9cacb725fe37eed7
katell <katell.fracassi@gmail.com>
2019-02-12 10:22:26 +0100

    footer resp

--- view/includes/footer.php
+++ view/includes/footer.php
@@ -1,14 +1,14 @@
 
 <footer id="footer">
 
-    <nav class="navbar  navbar-expand-lg navbar-dark nav_footer">
+    <nav class="navbar navbar-expand-lg navbar-dark nav_footer">
         <div class="container">
             <div class="navbar-collapse row justify-content-center" id="navbarTogglerDemo03">
-                <ul class="navbar-nav flex_rowR">
-                    <li class="nav-item active margin_rightR">
+                <ul class="navbar-nav footer_r">
+                    <li class="nav-item active">
                         <a class="nav-link" href="#">Legal</a>
                     </li>
-                    <li class="nav-item active margin_rightR">
+                    <li class="nav-item active marginLeft_r">
                         <a class="nav-link" href="index.php?action=contact">Contact</a>
                     </li>
                 </ul>
