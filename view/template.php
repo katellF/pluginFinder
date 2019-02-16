cd833add7f4d2a22c7c1f8c785fea5b12073701a
katell <katell.fracassi@gmail.com>
2019-02-16 16:19:58 +0100

    templates updated

--- view/template.php
+++ view/template.php
@@ -25,29 +25,28 @@
 <header>
     <nav class="navbar header__nav navbar-expand-lg navbar-dark">
         <div class="container">
-
             <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
                     aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div>
-                <img src="/pluginfinder/public/img/logo_4.png" alt="logo" height="100" width="100"/>
+                <img src="../pluginfinder/public/img/logo_1.png" alt="website logo" height="90" width="90"/>
             </div>
             <div class=" collapse navbar-collapse row menu menu_burger" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mt-2 mt-lg-0">
-                    <li class="nav-item active border_nav color_link bar">
-                        <a class="nav-link color_link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item active border_nav bar">
+                        <a class="nav-link nav_linkR" href="index.php">Home <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item active border_nav color_link bar">
-                        <a class="nav-link color_link" href="index.php?action=info">About</a>
+                    <li class="nav-item active border_nav bar">
+                        <a class="nav-link nav_linkR" href="index.php?action=info">About</a>
                     <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=contact">Contact</a>
-                    </li>
-                    <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=connect">Registration</a>
+                        <a class="nav-link nav_linkR" href="index.php?action=contact">Contact</a>
                     </li>
                     <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=connect/connection">Login</a>
+                        <a class="nav-link nav_linkR" href="index.php?action=connect">Registration</a>
+                    </li>
+                    <li class="nav-item active bar">
+                        <a class="nav-link nav_linkR" href="index.php?action=connect/connection">Login</a>
                     </li>
                 </ul>
             </div>
--- view/template_member.php
+++ view/template_member.php
@@ -31,27 +31,27 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div>
-                <img src="../pluginfinder/public/img/logo_1.png" alt="logo" height="90" width="90"/>
+                <img src="../pluginfinder/public/img/logo_1.png" alt="website logo" height="90" width="90"/>
             </div>
             <div class=" collapse navbar-collapse row menu menu_burger" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mt-2 mt-lg-0">
-                    <li class="nav-item active border_nav color_link bar">
-                        <a class="nav-link color_link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item active border_nav bar">
+                        <a class="nav-link nav_linkR" href="index.php">Home <span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item active border_nav color_link bar">
-                        <a class="nav-link color_link" href="index.php?action=info">About</a>
-                    </li>
-                    <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=contact">Contact</a>
+                    <li class="nav-item active border_nav bar">
+                        <a class="nav-link nav_linkR" href="index.php?action=info">About</a>
                     </li>
                     <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=member">Favorites</a>
+                        <a class="nav-link nav_linkR" href="index.php?action=contact">Contact</a>
                     </li>
                     <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=connect/modifypassword">Account</a>
+                        <a class="nav-link nav_linkR" href="index.php?action=member">Favorites</a>
                     </li>
                     <li class="nav-item active bar">
-                        <a class="nav-link color_link" href="index.php?action=connect/logout">Logout</a>
+                        <a class="nav-link nav_linkR" href="index.php?action=connect/modifypassword">Account</a>
+                    </li>
+                    <li class="nav-item active bar">
+                        <a class="nav-link nav_linkR" href="index.php?action=connect/logout">Logout</a>
                     </li>
                 </ul>
             </div>
