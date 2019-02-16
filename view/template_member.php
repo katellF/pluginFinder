6cd954624d1c9d9b689e720cb2622424830d44c9
katell <katell.fracassi@gmail.com>
2019-02-16 15:48:36 +0100

    bug menu_burger fixed

--- view/template_member.php
+++ view/template_member.php
@@ -33,7 +33,7 @@
             <div>
                 <img src="../pluginfinder/public/img/logo_1.png" alt="logo" height="90" width="90"/>
             </div>
-            <div class=" collapse navbar-collapse row justify-content-end menu_burger" id="navbarTogglerDemo03">
+            <div class=" collapse navbar-collapse row menu menu_burger" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mt-2 mt-lg-0">
                     <li class="nav-item active border_nav color_link bar">
                         <a class="nav-link color_link" href="index.php">Home <span class="sr-only">(current)</span></a>
