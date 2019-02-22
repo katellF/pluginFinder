94ac168830f3ddd52c573dd048e0910263427748
katell <katell.fracassi@gmail.com>
2019-02-22 13:34:42 +0100

    responsif adjusted nav

--- view/template.php
+++ view/template.php
@@ -36,7 +36,7 @@
             <div class=" collapse navbar-collapse row menu menu_burger" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mt-2 mt-lg-0">
                     <li class="nav-item active border_nav bar">
-                        <a class="nav-link nav_linkR" href="index.php">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link nav_linkR" href="index.php">Home<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item active border_nav bar">
                         <a class="nav-link nav_linkR" href="index.php?action=info">About</a>
